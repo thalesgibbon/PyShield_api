eb07f45be9538d1e8dd7447438d8422e3cafdfa1
thalesgibbon <thales.gibbon@gmail.com>
2018-11-28 15:56:57 -0200

    api score devedor

--- app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Py_Log_mssql
-
-
-class Py_LogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Py_Log_mssql
-        fields = '__all__'
--- app/views.py
+++ app/views.py
@@ -1,34 +1,37 @@
 from django.shortcuts import render
-from .models import Py_Log_mssql
-from .serializers import Py_LogSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from __key__ import ip
 
 
-class Py_LogList(APIView):
-    def get(self, request, pylog_id):
-        comments = Py_Log_mssql.objects.filter(pylog_id=pylog_id).values()
-        serializer = Py_LogSerializer(comments, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-
 class simula_score_devedor(APIView):
-    def get(self, request, id_credor, id_pessoa, id_tipo_garantia, id_cep, valor_pmt):
+    def get(self, request):
         import pickle
         filename = r'\\' + ip['ip_thanus_fonte'] + r"\ProjetosDW\data\load\modelo.app_score_devedor.pkl"
 
-        credor = int(id_credor)
-        pessoa = int(id_pessoa)
-        garantia = int(id_tipo_garantia)
-        cep = int(id_cep)
-        valor = float(valor_pmt)
+        credor = int(request.GET['id_credor'])
+        pessoa = int(request.GET['id_pessoa'])
+        garantia = int(request.GET['id_tipo_garantia'])
+        cep = int(request.GET['id_cep'])
+        valor = float(request.GET['valor_pmt'])
 
         loaded_model = pickle.load(open(filename, 'rb'))
         retorno = loaded_model.predict([[credor, pessoa, garantia, cep, valor]])[0].astype(int)
 
-        return Response(data=retorno, status=status.HTTP_200_OK)
+        dicio = {}
+        dicio['valor'] = retorno
+        def depara(x):
+            if x <= 30:
+                var = '0 ~ 30 dias'
+            elif x <= 180:
+                var = '30 ~ 180 dias'
+            else:
+                var = 'maior que 180 dias'
+            return var
+        dicio['classificacao'] = depara(retorno)
+
+        return Response(data=dicio, status=status.HTTP_200_OK)
 
 
 def home(request):
