191d69fda08f91ae4aa4289f100004cfff72b07c
thalesgibbon <thales.gibbon@gmail.com>
2018-11-27 23:13:05 -0200

    api score devedor

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
-from .views import Py_LogList
+from .views import Py_LogList, simula_score_devedor
 
 urlpatterns = [
     url(r'^logs/?$', Py_LogList.as_view(), name='logs'),
     url(r'^log/(?P<pylog_id>[0-9]+)/?$', Py_LogList.as_view(), name='log'),
+    #
+    url(r'^credor=(?P<id_credor>[0-9]+)' +
+        r'&pessoa=(?P<id_pessoa>[0-9]+)' +
+        r'&garantia=(?P<id_tipo_garantia>[0-9]+)' +
+        r'&cep_uf=(?P<id_cep>[0-9]+)' +
+        r'&valor=(?P<valor_pmt>[0-9]+)' +
+        r'/?$', simula_score_devedor.as_view(), name='simula_score_devedor'),
     ]
--- app/views.py
+++ app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Py_Log_mssql
 from .serializers import Py_LogSerializer
 from rest_framework.views import APIView
-
 from rest_framework.response import Response
 from rest_framework import status
+from __key__ import ip
 
 
 class Py_LogList(APIView):
@@ -12,6 +12,23 @@ class Py_LogList(APIView):
         comments = Py_Log_mssql.objects.filter(pylog_id=pylog_id).values()
         serializer = Py_LogSerializer(comments, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class simula_score_devedor(APIView):
+    def get(self, request, id_credor, id_pessoa, id_tipo_garantia, id_cep, valor_pmt):
+        import pickle
+        filename = r'\\' + ip['ip_thanus_fonte'] + r"\ProjetosDW\data\load\modelo.app_score_devedor.pkl"
+
+        credor = int(id_credor)
+        pessoa = int(id_pessoa)
+        garantia = int(id_tipo_garantia)
+        cep = int(id_cep)
+        valor = float(valor_pmt)
+
+        loaded_model = pickle.load(open(filename, 'rb'))
+        retorno = loaded_model.predict([[credor, pessoa, garantia, cep, valor]])[0].astype(int)
+
+        return Response(data=retorno, status=status.HTTP_200_OK)
 
 
 def home(request):
