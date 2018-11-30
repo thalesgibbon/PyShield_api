3075f0ac3fccbbacd9386f1962770095346e20f4
thalesgibbon <thales.gibbon@gmail.com>
2018-11-30 14:43:37 -0200

    api score devedor

--- app/views.py
+++ app/views.py
@@ -3,11 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from __key__ import ip
+import pickle
 
 
 class simula_score_devedor(APIView):
     def get(self, request):
-        import pickle
+
         filename = r'\\' + ip['ip_thanus_fonte'] + r"\ProjetosDW\data\load\modelo.app_score_devedor.pkl"
 
         credor = int(request.GET['id_credor'])
