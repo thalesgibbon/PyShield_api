faaa1b0764ef04e292589def2db3f6fe347f8e58
thalesgibbon <thales.gibbon@gmail.com>
2018-12-04 14:02:17 -0200

    api add busca telefone

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import simula_score_devedor
+from .views import simula_score_devedor, busca_telefone
 
 urlpatterns = [
     url(r'^simulacao', simula_score_devedor.as_view(), name='simulacao'),
+    url(r'^busca_telefone', busca_telefone.as_view(), name='busca_telefone'),
     ]
--- app/views.py
+++ app/views.py
@@ -3,8 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from __key__ import ip
+import pandas as pd
 import pickle
 
+full_tel = pd.read_pickle(r'\\' + ip['ip_thanus_fonte'] + r"\ProjetosDW\data\load\#telefone.pkl")
+full_score_tel = pd.read_pickle(r'\\' + ip['ip_thanus_fonte'] + r"\ProjetosDW\data\load\#app_score_telefone.pkl")
 
 class simula_score_devedor(APIView):
     def get(self, request):
@@ -31,6 +34,30 @@ class simula_score_devedor(APIView):
                 var = 'maior que 180 dias'
             return var
         dicio['classificacao'] = depara(retorno)
+
+        return Response(data=dicio, status=status.HTTP_200_OK)
+
+
+class busca_telefone(APIView):
+
+    def get(self, request):
+        cpfcnpj = int(request.GET['devedor_cpfcnpj'])
+
+        tel = full_tel.loc[full_tel.devedor_cpfcnpj == cpfcnpj, ['telefone_numero', 'telefone_data_cadastro', 'telefone_credor_origem']]
+        score_tel = full_score_tel.loc[full_score_tel.telefone_numero.isin(tel.telefone_numero), ['telefone_numero', 'score_group']]
+
+        merge = pd.merge(tel
+                         ,score_tel
+                         ,on='telefone_numero'
+                         ,how='left'
+                        )
+        merge['score_group'] = merge.score_group.fillna('N/A')
+        merge = merge.sort_values(['score_group', 'telefone_data_cadastro'], ascending=[True, False])
+
+        if len(merge) > 150:
+            dicio = {0: 'mais de 150 registros'}
+        else:
+            dicio = merge.to_dict('index')
 
         return Response(data=dicio, status=status.HTTP_200_OK)
 
